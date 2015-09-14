8ae31cedcba9b85a0069092fb7dc785065873c65
Mike Fielden <fielden.mike@gmail.com>
2015-09-14 11:58:55 -0500

    postinstall?

--- dist/getIgnoreFilePath.js
+++ dist/getIgnoreFilePath.js
@@ -12,25 +12,19 @@ var checkForProjectFile = require('./checkForProjectFile.js'),
     formatAngryText = require('./formatAngryText');
 
 module.exports = function (projectDir) {
-	var jshintIgnore = checkForProjectFile(projectDir, '.jshintignore'),
-	    defaultHintIgnore = checkForProjectFile(path.join(__dirname), '.jshintignore');
+  var jshintIgnore = checkForProjectFile(projectDir, '.jshintignore'),
+      defaultHintIgnore = checkForProjectFile(path.join(__dirname), '.jshintignore');
 
-	//console.log(`fn: ${__filename}`);
-	//console.log(`rps fn: ${fs.realpathSync(__filename)}`);
-	//console.log(`path dirname: ${path.dirname(fs.realpathSync(__filename))}`);
-	//console.log(`path join: ${path.join(path.dirname(fs.realpathSync(__filename)), '../configuration/.jshintignore')}`);
-	//console.log(`path resolve: ${path.resolve(path.dirname(fs.realpathSync(__filename)), '../configuration/.jshintignore')}`);
+  if (jshintIgnore) {
+    console.log('\nFound .jshintignore file...');
+    console.log('Using ' + formatHappyText(jshintIgnore) + ' as excludePath');
 
-	if (jshintIgnore) {
-		console.log('\nFound .jshintignore file...');
-		console.log('Using ' + formatHappyText(jshintIgnore) + ' as excludePath');
+    return jshintIgnore;
+  }
 
-		return jshintIgnore;
-	}
+  console.log('\n' + formatAngryText('Didnt find a .jshintignore file locally'));
+  console.log('Using ' + formatHappyText('default astro') + ' .jshintignore as exclude \n');
+  console.log('Using ' + formatHappyText(defaultHintIgnore) + ' as excludePath');
 
-	console.log('\n' + formatAngryText('Didnt find a .jshintignore file locally'));
-	console.log('Using ' + formatHappyText('default astro') + ' .jshintignore as exclude \n');
-	console.log('Using ' + formatHappyText(defaultHintIgnore) + ' as excludePath');
-
-	return defaultHintIgnore;
+  return defaultHintIgnore;
 };
